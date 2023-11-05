1c61b2def678c0a10dc0baae77fd5abb90ed3f1e
Apolo  Rossi <apolorossi@Apolos-MacBook-Pro.local>
2023-11-05 15:52:46 -0300

    feat: Implemented sign up to register a new user.

--- app/src/main/java/com/devtides/coroutinesroom/view/SignupFragment.kt
+++ app/src/main/java/com/devtides/coroutinesroom/view/SignupFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -35,17 +36,27 @@ class SignupFragment : Fragment() {
 
     private fun observeViewModel() {
         viewModel.signupComplete.observe(this, Observer { isComplete ->
-
+            Toast.makeText(activity, "Signup complete", Toast.LENGTH_SHORT).show()
+            val action  = SignupFragmentDirections.actionGoToMain()
+            Navigation.findNavController(binding.signupUsername).navigate(action)
         })
 
         viewModel.error.observe(this, Observer { error ->
-
+            Toast.makeText(activity, "Error: $error", Toast.LENGTH_SHORT).show()
         })
     }
 
     private fun onSignup(v: View){
-        val action = SignupFragmentDirections.actionGoToMain()
-        Navigation.findNavController(v).navigate(action)
+
+        val username = binding.signupUsername.text.toString()
+        val password = binding.signupPassword.text.toString()
+        val info = binding.otherInfo.text.toString()
+
+        if(username.isEmpty() || password.isEmpty() || info.isEmpty()){
+            Toast.makeText(activity, "Please fill all fields", Toast.LENGTH_SHORT).show()
+        } else {
+            viewModel.signup(username, password, info)
+        }
     }
 
     private fun onGotoLogin(v: View) {
--- app/src/main/java/com/devtides/coroutinesroom/viewmodel/SignupViewModel.kt
+++ app/src/main/java/com/devtides/coroutinesroom/viewmodel/SignupViewModel.kt
@@ -3,14 +3,35 @@ package com.devtides.coroutinesroom.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.devtides.coroutinesroom.model.LoginState
+import com.devtides.coroutinesroom.model.User
+import com.devtides.coroutinesroom.model.UserDataBase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class SignupViewModel(application: Application) : AndroidViewModel(application) {
 
     val signupComplete = MutableLiveData<Boolean>()
     val error = MutableLiveData<String>()
 
-    fun signup(username: String, password: String, info: String) {
+    private val db by lazy { UserDataBase(getApplication()).userDao() }
 
+    fun signup(username: String, password: String, info: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val user = db.getUser(username)
+            if(user != null) {
+                error.postValue("User already exists")
+                return@launch
+            } else {
+                val user = User(username, password.hashCode(), info)
+                val userId = db.insertUser(user)
+                user.id = userId
+                LoginState.login(user)
+                signupComplete.postValue(true)
+            }
+        }
     }
 
 }
